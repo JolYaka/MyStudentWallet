ddde3ecab45ddc04635c7a4d478363b9c80afec1
Michel Joël Boris Somyida YAKA <yak.wwwjjo@gmail.com>
2025-07-20 19:25:33 +0100

    Improvement of the TransListController class

--- src/main/java/com/example/mystudentwallet/TransListController.java
+++ src/main/java/com/example/mystudentwallet/TransListController.java
@@ -12,7 +12,8 @@ public class TransListController implements Initializable {
     @FXML
     Label descriptionLabel1,descriptionLabel2,descriptionLabel3,descriptionLabel4,dateLabel1,dateLabel2,dateLabel3,dateLabel4,amountLabel1,amountLabel2,amountLabel3,amountLabel4;
     public void initialize (URL location, ResourceBundle resource ){
-
+        setLabel();
+        sqlPart();
     }
     private void setLabel(){
         descriptionLabel1.setText("Desciption récente");
@@ -22,7 +23,27 @@ public class TransListController implements Initializable {
         dateLabel1.setText("XXXX-XX-XX");
         dateLabel2.setText("XXXX-XX-XX");
         dateLabel3.setText("XXXX-XX-XX");
-        dateLabel.setText("XXXX-XX-XX");
-
+        dateLabel4.setText("XXXX-XX-XX");
+        amountLabel1.setText("XXXX");
+        amountLabel2.setText("XXXX");
+        amountLabel3.setText("XXXX");
+        amountLabel4.setText("XXXX");
+    }
+    private void sqlPart(){
+        try{
+        String url = "jdbc:sqlite:src/main/resources/com/example/mystudentwallet/myStudentWalletBD.db";
+        String sql = "SELECT description,amount,date FROM Transactions ORDER BY id DESC LIMIT 4";
+        Connection conn = DriverManager.getConnection(url);
+        PreparedStatement pstmt= conn.prepareStatement(sql);
+        ResultSet rs = pstmt.executeQuery();
+        while(rs.next()){
+            descriptionLabel1.setText(rs.getString("description"));
+            amountLabel1.setText(String.format("%.2f",rs.getDouble("amount")));
+            dateLabel1.setText(rs.getString("date"));
+        }
+        }
+        catch(SQLException e){
+            System.err.println("Le message d'erreur est:"+e.getMessage());
+        }
     }
 }
